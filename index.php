94ad6773d09ee1c2bbb4774225d10ec8780a8d3e
gocapuskarevic <gordana@axiomq.com>
2020-09-16 19:04:43 +0200

    Created 20 pdfs in single click.

--- index.php
+++ index.php
@@ -5,47 +5,53 @@ require_once 'vendor/autoload.php';
 use Dompdf\Dompdf;
 
 
-$dompdf = new Dompdf();
-$dompdf->set_option('chroot', 'C:\wamp64\www\vtldiplome');
-$dompdf->set_option('defaultMediaType', 'all');
-$dompdf->set_option('isFontSubsettingEnabled', true);
-
-//$html = file_get_contents('diploma.html');
-
-$html = '
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" media="all" href="styles.css" />
-  </head>
-  <body>
-    <div class="wrapper">
-      <div class="content">
-        <div class="logo">
-          <img src="vtl.png" alt="" class="left">
-          <img src="bukovac.png" alt="" class="right">
+$fn = fopen('takmicari.csv',"r");
+  while (!feof($fn)) {
+    $dompdf = new Dompdf();
+    $dompdf->set_option('chroot', 'C:\wamp64\www\vtldiplome');
+    $dompdf->set_option('defaultMediaType', 'all');
+    $tmp = explode(',',fgets($fn));
+    $name = $tmp[2];
+    $length = $tmp[11];
+    $time = $tmp[5];
+    $html = '
+    <html>
+      <head>
+        <link rel="stylesheet" type="text/css" media="all" href="styles.css" />
+      </head>
+      <body>
+        <div class="wrapper">
+          <div class="content">
+            <div class="logo">
+              <img src="vtl.png" alt="" class="left">
+              <img src="bukovac.png" alt="" class="right">
+            </div>
+            <h1>DIPLOMA</h1>
+            <h2>'.$name.'</h2>
+            <div class="data">
+              <p class="left">Staza : '.$length.'</p>
+              <p class="right">Vreme : '.$time.'</p>
+            </div>
+            <div class="date">
+              <p>Bukovac, 12.09.2020.</p>
+            </div>
+          </div>
         </div>
-        <h1>DIPLOMA</h1>
-        <h2>Goran Žigić</h2>
-        <div class="data">
-          <p class="left">Staza : 35.7km</p>
-          <p class="right">Vreme : 08:35:22</p>
-        </div>
-        <div class="date">
-          <p>Bukovac, 12.09.2020.</p>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>';
+      </body>
+    </html>';
 
-$dompdf->loadHtml($html);
+    $dompdf->loadHtml($html);
 
+    $dompdf->setPaper('A4', 'landscape');
 
-$dompdf->setPaper('A4', 'landscape');
+    $dompdf->render();
+
+    $output_pdf = $dompdf->output();
+
+    file_put_contents('diplome/'.$name.'.pdf', $output_pdf);
+    unset($dompdf);
+}
+fclose($fn); 
 
 
-$dompdf->render();
-
-
-$dompdf->stream('name',array('Attachment'=>0));
 ?> 
