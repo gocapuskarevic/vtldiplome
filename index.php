46461c09197b518b30c85e7873eef94a0a268c12
gocapuskarevic <gordana@axiomq.com>
2020-09-17 19:32:42 +0200

    Add event info.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'vendor/autoload.php';
 
 use Dompdf\Dompdf;
 
+$kolo = 'za učešće u V kolu Vojvođanske Treking Lige';
 
 $dompdf = new Dompdf();
 $dompdf->set_option('chroot', 'C:\wamp64\www\vtldiplome');
@@ -12,9 +13,10 @@ $dompdf->set_option('isFontSubsettingEnabled', true);
 
 //$html = file_get_contents('diploma.html');
 
-$html = '<html>
+$html = '<!DOCTYPE html>
 <head>
   <link rel="stylesheet" type="text/css" media="all" href="styles.css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
   <div class="wrapper">
@@ -24,6 +26,7 @@ $html = '<html>
         <img src="bukovac.png" alt="" class="right">
       </div>
       <h1>DIPLOMA</h1>
+      <h3>za učešće u V kolu Vojvođanske Treking Lige</h3>
       <h2>Goran Žigić</h2>
       <div class="data">
         <p class="left">Staza : 35.7km</p>
